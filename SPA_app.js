2dd56234d5dc6c4437cb0a65f2489ec5850c9673
May <rlaguswn01523@gmail.com>
2025-07-29 23:37:18 +0800

    SPA #2

--- SPA_app.js
+++ SPA_app.js
@@ -1,6 +1,24 @@
 const app = Vue.createApp({
     // data, functions
     // template: '<h2> I am a template </h2>'
+
+    data() {
+        return {
+            showPerson: true,
+            person: 'KIM Hyunjoo',
+            age: 24,
+            nationality: 'South Korea',
+        }
+    },
+    methods: {
+        changePerson(person) {
+            // this.person = 'KIM Ejoo'
+            this.person = person
+        },
+        toggleShowBox() {
+            this.showPerson = !this.showPerson
+        }
+    }
 })
 
 app.mount('#app')
