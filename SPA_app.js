d032555aa3f0bdc64d0c7c11f6ff108c61054dc3
May <rlaguswn01523@gmail.com>
2025-07-30 12:04:20 +0800

    SPA #3

--- SPA_app.js
+++ SPA_app.js
@@ -4,10 +4,19 @@ const app = Vue.createApp({
 
     data() {
         return {
+            url: 'https://www.naver.com/',
             showPerson: true,
             person: 'KIM Hyunjoo',
             age: 24,
             nationality: 'South Korea',
+            x: 0,
+            y: 0,
+            showBooks: true,
+            books: [
+                {title: 'Five People You Meet in Heaven', author: 'Mitch Albom', img: 'assets/1.jpg', isFav: true},
+                {title: 'Blindness', author: 'Jose Saramago', img: 'assets/2.jpg', isFav: false},
+                {title: 'Empire of the Ants', author: 'Bernard Weber', img: 'assets/3.jpg', isFav: true},
+            ]
         }
     },
     methods: {
@@ -17,12 +26,28 @@ const app = Vue.createApp({
         // },
         toggleShowBox() {
             this.showPerson = !this.showPerson
+            this.showBooks = !this.showBooks
         },
-        handleEvent() {
-            console.log('HELLLLLOOOOOOOO')
+        toggleBook(book) {
+            book.isFav = !book.isFav
         },
-        alertEvent() {
-            alert('AAAAAAAAAAAAAAAAAaa')
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+        // alertEvent() {
+        //     alert('AAAAAAAAAAAAAAAAAaa')
+        // }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
